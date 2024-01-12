cb046ecfd9ce5f427736bfbd6c12896cefed5eb4
Nathan <117750247+Nathan3-14@users.noreply.github.com>
2024-01-12 22:05:06 +0000

    codespace changes

--- homework/11-dictionaries/main.py
+++ homework/11-dictionaries/main.py
@@ -9,6 +9,33 @@ colour_just = item_just + 10
 display_shop = {
     f"{item.split('_')[0].ljust(item_just).capitalize()} {item.split('_')[1].capitalize()}": price for item, price in shop.items()
 }
+cart = []
+codes = {
+    "50off": 0.5,
+    "50on": 1.5,
+    "free": 0
+}
 
-print(" - Item".ljust(item_just+4) + "Pattern".ljust(item_just+1) + "Price" + "\n")
-print("".join(f" - {str((item+':')).ljust(colour_just)} £{format(price/100, '.2f')}\n" for item, price in display_shop.items()))
+while True:
+    print(" - Item".ljust(item_just+4) + "Pattern".ljust(item_just+1) + "Price  "  + "\n")
+    print("".join(f" - {str((item+':')).ljust(colour_just)} £{format(price/100, '.2f')}   ({list(shop.keys())[list(display_shop.keys()).index(item)]})\n" for item, price in display_shop.items()))
+
+    buying = input("Enter an item to buy or exit to exit\n>> ").lower()
+
+    if buying == "exit":
+        break
+
+    if buying in shop:
+        cart.append(shop[buying])
+    else:
+        print("Item not recognised")
+
+total = 0
+for cost in cart:
+    total += cost
+
+code = input("Enter any discount codes you may have\n>> ")
+if code in codes.keys():
+    total *= codes[code]
+
+print(f"Total: £{format(total/100, '.2f')}")
