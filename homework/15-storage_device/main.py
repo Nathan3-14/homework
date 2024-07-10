390b1fb1218dd0405b1d6ba8e93af1eea906a575
Nathan3-14 <nathan.watson.172@gmail.com>
2024-07-10 21:07:43 +0100

    Changed values to not rated

--- homework/15-storage_device/main.py
+++ homework/15-storage_device/main.py
@@ -4,24 +4,36 @@ from rich.prompt import Prompt
 console = Console()
 
 types = {
+    "magnetic": {
+        "capacity": 3,
+        "access_speed": 2,
+        "portability": 3,
+        "durability": 1,
+        "reliability": 3,
+        "cost": 2
+    },
     "solid state": {
+        "capacity": 2,
+        "access_speed": 3,
+        "portability": 3,
         "durability": 3,
         "reliability": 3,
         "cost": 1
     },
-    "magnetic": {
-        "durability": 2,
-        "reliability": 2,
-        "cost": 2
-    },
     "optical": {
-        "durability": 1,
-        "reliability": 1,
+        "capacity": 1,
+        "access_speed": 1,
+        "portability": 3,
+        "durability": 2,
+        "reliability": 3,
         "cost": 3
     }
 }
 
 needs = {
+    "capacity": Prompt.ask("Do you need capacity", choices=["y", "n"]),
+    "access_speed": Prompt.ask("Do you need access speed", choices=["y", "n"]),
+    "portability": Prompt.ask("Do you need portability", choices=["y", "n"]),
     "durability": Prompt.ask("Do you need durability", choices=["y", "n"]),
     "reliability": Prompt.ask("Do you need reliability", choices=["y", "n"]),
     "cost": Prompt.ask("Do you need cost", choices=["y", "n"])
